10869d97d5e48d7ccf871e6a0e5f477a4bfda2ce
Søren Howe Gersager <syrelyre@gmail.com>
2014-04-24 12:14:29 +0200

    schema changes

--- REST/schema.sql
+++ REST/schema.sql
@@ -1,13 +1,38 @@
+-- delete existing tables
+drop table if exists toilets;
+drop table if exists toilet_group;
+
+-- create schema
 create table toilets (
 id integer primary key,
+group_id integer,
 description text,
 location text,
 occupied integer,
 methane_level integer,
 lat real,
+lng real,
+FOREIGN KEY(group_id) references toilet_group(id)
+);
+create table toilet_group (
+id integer primary key,
+name text,
+lat real,
 lng real
 );
-insert into toilets (id, description, lat, lng, location, occupied, methane_level)
-	values (NULL, "Toilet ved grønnegade", 55.722353, 12.54459, "grønnegade 1", 0, 50);
-insert into toilets (id, description, lat, lng, location, occupied, methane_level)
-	values (NULL, "Toilet ved skippervej", 55.722353, 12.54459, "skippervej 1 1", 1, 20);
+
+--- insert data
+insert into toilet_group (id, name, lat, lng) values (1, "Test Ø 1", 55.621727, 12.078388);
+
+insert into toilets (id, group_id, description, lat, lng, location, occupied, methane_level)
+	values (NULL, 1,"Toilet 1 ved Test Ø 1", 55.621803, 12.078233, "Roskilde Festival", 0, 50);
+insert into toilets (id, group_id, description, lat, lng, location, occupied, methane_level)
+	values (NULL, 1, "Toilet 2 ved Test Ø 1", 55.621803, 12.078362, "Roskilde Festival", 1, 30);
+insert into toilets (id, group_id, description, lat, lng, location, occupied, methane_level)
+    values (NULL, 1, "Toilet 3 ved Test Ø 1", 55.621786, 12.078533, "Roskilde Festival", 0, 20);
+insert into toilets (id, group_id, description, lat, lng, location, occupied, methane_level)
+    values (NULL, 1, "Toilet 4 ved Test Ø 1", 55.621580, 12.078214, "Roskilde Festival", 1, 10);
+insert into toilets (id, group_id, description, lat, lng, location, occupied, methane_level)
+    values (NULL, 1, "Toilet 5 ved Test Ø 1", 55.621609, 12.078337, "Roskilde Festival", 0, 90);
+insert into toilets (id, group_id, description, lat, lng, location, occupied, methane_level)
+    values (NULL, 1, "Toilet 6 ved Test Ø 1", 55.621576, 12.078576, "Roskilde Festival", 1, 65);
