b6e850b23ba30f92e2c2dad5b3adb23435625ac4
asger <asger@asgerha.dk>
2014-05-09 18:44:30 +0200

    test

--- App/app/build/source/r/debug/com/example/toiletfinder/R.java
+++ App/app/build/source/r/debug/com/example/toiletfinder/R.java
@@ -206,19 +206,26 @@ containing a value of this type.
         public static final int common_signin_btn_text_normal_light=0x7f020015;
         public static final int common_signin_btn_text_pressed_dark=0x7f020016;
         public static final int common_signin_btn_text_pressed_light=0x7f020017;
-        public static final int ic_launcher=0x7f020018;
-        public static final int ic_plusone_medium_off_client=0x7f020019;
-        public static final int ic_plusone_small_off_client=0x7f02001a;
-        public static final int ic_plusone_standard_off_client=0x7f02001b;
-        public static final int ic_plusone_tall_off_client=0x7f02001c;
-        public static final int toilet=0x7f02001d;
+        public static final int ic_action_refresh=0x7f020018;
+        public static final int ic_launcher=0x7f020019;
+        public static final int ic_plusone_medium_off_client=0x7f02001a;
+        public static final int ic_plusone_small_off_client=0x7f02001b;
+        public static final int ic_plusone_standard_off_client=0x7f02001c;
+        public static final int ic_plusone_tall_off_client=0x7f02001d;
+        public static final int toilet=0x7f02001e;
+        public static final int toiletfree=0x7f02001f;
+        public static final int toiletocc=0x7f020020;
+        public static final int toiletunknown=0x7f020021;
     }
     public static final class id {
-        public static final int action_settings=0x7f05000f;
+        public static final int action_refresh_10=0x7f050010;
+        public static final int action_refresh_5=0x7f05000f;
+        public static final int action_refresh_real_time=0x7f050011;
         public static final int buttonframe=0x7f05000b;
         public static final int fetchtoiletsbutton=0x7f05000c;
         public static final int hybrid=0x7f050004;
-        public static final int map=0x7f05000e;
+        public static final int map=0x7f05000d;
+        public static final int map_text_view=0x7f05000e;
         public static final int methane_level_bar=0x7f050009;
         public static final int none=0x7f050000;
         public static final int normal=0x7f050001;
@@ -229,7 +236,6 @@ containing a value of this type.
         public static final int terrain=0x7f050003;
         public static final int titleTextView=0x7f050007;
         public static final int toiletlistframe=0x7f05000a;
-        public static final int toiletmapfragment=0x7f05000d;
     }
     public static final class integer {
         public static final int google_play_services_version=0x7f070000;
