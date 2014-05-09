fc42119c7fc825316542dc78c66795036f395299
Søren Howe Gersager <syrelyre@gmail.com>
2014-05-09 14:20:30 +0200

    gave the exchange a more meaningful name

--- web/publisher.py
+++ web/publisher.py
@@ -8,9 +8,9 @@ cred = pika.PlainCredentials("node", "node")
 connection = pika.BlockingConnection(pika.ConnectionParameters("127.0.0.1",credentials=cred))
 
 channel = connection.channel()
-channel.exchange_declare(exchange="logs", type="fanout")
+channel.exchange_declare(exchange="toilet_events", type="fanout")
 message = {"group_id":1, "toilet_id": 2, "occupied":False, "methane_level": 20, "time_stamp": str(datetime.datetime.now())}
 
-channel.basic_publish(exchange="logs",routing_key="", body=json.dumps(message))
+channel.basic_publish(exchange="toilet_events",routing_key="", body=json.dumps(message))
 
 connection.close()
